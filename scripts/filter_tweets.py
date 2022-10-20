d2399e358b7123224fa31bc2f00d21137ff86281
FoxyLuxe <foxscripter@gmail.com>
2022-10-20 17:16:03 -0500

    filter updated

--- scripts/filter_tweets.py
+++ scripts/filter_tweets.py
@@ -42,6 +42,7 @@ import pandas as pd
 from config import guest
 from src.data.miners import TweetParser
 from argparse import ArgumentParser
+from datetime import datetime as dt
 
 def make_arguments():
     parser = ArgumentParser(description="tweets filter pipeline")
@@ -52,7 +53,7 @@ def make_arguments():
     return parser.parse_args()
 
 def load_file(file: str, group = "raw"):
-    return pd.read_csv(f"data/{group}/{file}")
+    return pd.read_csv(f"data/{group}/{file}", index_col=0)
 
 def make_filename(args: ArgumentParser):
     def rewrite(obj: list):
@@ -67,7 +68,7 @@ def make_filename(args: ArgumentParser):
     if args.match != []:
         filename += f"_[{rewrite(args.match)}]"
     if args.date != []:
-        filename += f"_[{args.date[0]}|{args.date[1]}]"
+        filename += f"_[{args.date[0]}&{args.date[1]}]"
     filename += ".csv"
     print(f"transformer: match == {args.match}, date == {args.date}")
     print(f"--> pointing to {filename}")
@@ -75,6 +76,14 @@ def make_filename(args: ArgumentParser):
 
 def filter_data(data: pd.DataFrame, args):
     print("--> filtering match & dates")
+    if args.match != []:
+        data['match'] = data['text'].str.findall('(' + '|'.join(args.match) + ')')
+        data = data[data['match'].astype(bool)]
+    #Wed Oct 19 18:38:45 +0000 2022
+    #%a %b %d %H:%M:%S %z %Y
+    if args.date != []:
+        data['date'] = pd.to_datetime(data['date'])
+        data = data[(data['date'] > args.date[0]) & (data['date'] < args.date[1])]
     return data
 
 def save(data, file):
@@ -88,5 +97,5 @@ if __name__ == "__main__":
     data = load_file(args.file, group=args.group)
     name = make_filename(args)
     new_data = filter_data(data, args)
-    save(data, name)
+    save(new_data, name)
 
