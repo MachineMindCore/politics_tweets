775a807b5103ff4ebadfcbf641156a3de0d4941c
FoxyLuxe <foxscripter@gmail.com>
2022-10-22 15:04:36 -0500

    analitycs models structured

---
+++ src/data/analytics.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+class TimeLapsePlot:
+
+    @staticmethod
+    def label_tracing():
+        return
+    
+    @staticmethod
+    def match_tracing():
+        return
+
+class StatisticsPrinter:
+
+    @staticmethod
+    def label_mean():
+        return
