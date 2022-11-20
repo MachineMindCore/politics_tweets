a7fb135e819dae56238dbd98808cb9c1f99a920a
FoxyLuxe <foxscripter@gmail.com>
2022-11-20 16:37:43 -0500

    build updated

--- build/lib/tweet_toolkit/helpers/handle.py
+++ build/lib/tweet_toolkit/helpers/handle.py
@@ -4,4 +4,4 @@ def remove_url(text):
     return sub(r'http\S+', '', text)
 
 def date_hours(date):
-    return 8766*date.year + 730*date.month + 24*date.day
+    return 8766*date.year + 730*date.month + 24*date.day 
