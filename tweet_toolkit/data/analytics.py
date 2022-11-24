429bdd671964b64b6881aebf159ddaf2301659fe
FoxyLuxe <foxscripter@gmail.com>
2022-11-23 21:16:20 -0500

    distribution added

--- tweet_toolkit/data/analytics.py
+++ tweet_toolkit/data/analytics.py
@@ -2,6 +2,50 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import datetime  as dt
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from collections import Counter
+from itertools import islice
+import textacy.preprocessing.normalize as norm
+import textacy.preprocessing.remove as remv
+#lemma y frecuencia de uso por palabra no match (grafico-frecuencia) 10p, remover stopwords
+    #subinstancia: la veces que se mencionan palabras especificas (grafico-frecuencia)
+
+#grafico de referencias
+
+class FrecuencyPlot:
+
+    @staticmethod
+    def general_distribution(data, targets=None):
+        lemmatizer = WordNetLemmatizer()
+        transformation = []
+        trash_words = stopwords.words('spanish')
+        trash_words.append('')
+        trash_words.append(' ')
+        for raw in data["text"]:
+            tweet = norm.whitespace(remv.punctuation(raw))
+            if (targets == None) or (True in [target in tweet for target in targets]):
+                for word in tweet.split(" "):
+                    transformation.append(lemmatizer.lemmatize(word))
+        register = Counter(transformation)
+
+        filtered = {}
+        for k in register.keys():
+            if k not in trash_words:
+                filtered[k] = register[k]
+        
+        ordered = sorted(filtered.items(), key=lambda item: item[1], reverse=True)
+        for items in ordered[:10]:
+            plt.bar(items[0], items[1], color="black", width=0.5)
+        plt.xticks(rotation=90)
+        plt.show()
+        return dict(ordered[:10])
+
+    
+    @staticmethod
+    def set_distribution():
+        return
+
 class TimeLapsePlot:
 
     @staticmethod
@@ -23,9 +67,6 @@ class TimeLapsePlot:
         plt.show()
         return 
     
-    @staticmethod
-    def match_tracing(file):
-        return
 
 class StatisticsPrinter:
 
